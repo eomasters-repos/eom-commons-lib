03ac12191b205888af56897d5e9c8e7d4a706296
Marco <info@eomasters.org>
2024-02-06 08:34:57 +0100

    moved play method

--- src/main/java/org/eomasters/audio/Audio.java
+++ src/main/java/org/eomasters/audio/Audio.java
@@ -17,7 +17,16 @@
 
 package org.eomasters.audio;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class Audio {
 
@@ -29,8 +38,37 @@ public class Audio {
 
   public void play()  {
     try {
-      Audios.play(inputStream);
+      play(inputStream);
     } catch (AudioException ignore) {
+    }
+  }
+
+  /**
+   * Plays the given audio input stream. Supported formats are: WAV, AU SND, and AIFF.
+   *
+   * @param audioInput the audio input stream
+   * @throws AudioException if the audio input stream cannot be played
+   * @see javax.sound.sampled.AudioFileFormat.Type
+   */
+  public static void play(InputStream audioInput) throws AudioException {
+    try (AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioInput)) {
+      // Executor service is used to play the audio in a separate thread. This is necessary because the audio
+      // clip would be terminated immediately. LineListener is used to close the audio clip after it has been played.
+      // if executor would be a static field, it would prevent closing the main thread if not shutdown.
+      ExecutorService executor = Executors.newSingleThreadExecutor();
+
+      Clip clip = AudioSystem.getClip();
+      clip.open(audioStream);
+      clip.addLineListener(event -> {
+        if (event.getType() == LineEvent.Type.STOP) {
+          clip.close();
+          executor.shutdown();
+        }
+      });
+
+      executor.submit(clip::start);
+    } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+      throw new AudioException(e);
     }
   }
 }
