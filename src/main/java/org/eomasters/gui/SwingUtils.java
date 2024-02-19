c1d0bb39b7d01ca3c66bcc329e52a86b4c791627
Marco <info@eomasters.org>
2024-02-19 12:01:15 +0100

    updated SwingUtils class

--- src/main/java/org/eomasters/gui/SwingUtils.java
+++ src/main/java/org/eomasters/gui/SwingUtils.java
@@ -1,20 +1,42 @@
+/*-
+ * ========================LICENSE_START=================================
+ * EOM Commons - Library of common utilities for Java
+ * -> https://www.eomasters.org/
+ * ======================================================================
+ * Copyright (C) 2023 - 2024 Marco Peters
+ * ======================================================================
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * =========================LICENSE_END==================================
+ */
+
 package org.eomasters.gui;
 
 import java.awt.Component;
 import java.awt.Container;
+import java.awt.Dimension;
+import javax.swing.JComponent;
 
 public class SwingUtils {
 
-  public static void enableComponents(Container parent, boolean enable) {
-    Component[] components = parent.getComponents();
-    parent.setEnabled(enable);
-    for (Component component : components) {
+  public static void enableChildComponents(Container parent, boolean enable) {
+    for (Component component : parent.getComponents()) {
       if (component instanceof Container) {
-        enableComponents((Container) component, enable);
-      } else {
-        component.setEnabled(enable);
+        enableChildComponents((Container) component, enable);
       }
+      component.setEnabled(enable);
     }
+  }
+
+  private void updatePreferredWidth(JComponent component, int width) {
+    Dimension preferredSize = component.getPreferredSize();
+    preferredSize.width = width;
+    component.setPreferredSize(preferredSize);
   }
 
 }
