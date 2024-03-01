5fb0920d6c238903a0e813972b584f374d6f5712
Marco <info@eomasters.org>
2024-03-01 13:42:33 +0100

    added test

---
+++ src/test/java/org/eomasters/utils/TextUtilsTest.java
@@ -0,0 +1,38 @@
+/*-
+ * ========================LICENSE_START=================================
+ * EOM Commons - Library of common utilities for Java
+ * -> https://www.eomasters.org/
+ * ======================================================================
+ * Copyright (C) 2023 - 2024 Marco Peters
+ * ======================================================================
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * =========================LICENSE_END==================================
+ */
+
+package org.eomasters.utils;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class TextUtilsTest {
+
+  @Test
+  void testInvalidMailAddresses() {
+    assertFalse(TextUtils.isValidEmailAddress("illegal@@email.com"));
+    assertFalse(TextUtils.isValidEmailAddress("illegal@email"));
+  }
+
+  @Test
+  void testValidMailAddresses() {
+    assertTrue(TextUtils.isValidEmailAddress("marco@eomasters.org"));
+  }
+
+}
