7563fe45ea643dfd8929968221ad224ce87bc7ed
Marco <info@eomasters.org>
2024-05-10 11:05:15 +0200

    refactored the Highlighter class; now it is possible to manually clear the highlighting and the Highlighter must be constructed for component

--- src/main/java/org/eomasters/gui/Highlighter.java
+++ src/main/java/org/eomasters/gui/Highlighter.java
@@ -19,6 +19,9 @@ package org.eomasters.gui;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -27,46 +30,135 @@ import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.MatteBorder;
 
+/**
+ * A utility class for highlighting components. A border is drawn around the component to indicate that the component is
+ * highlighted and optionally a message is displayed.
+ */
 public class Highlighter {
 
-  private Border oldBorder;
-  private Color color = Color.red.darker();
-  private int margin = 2;
-  private double duration = 2.0;
-  private JComponent theComponent;
-  private String infoMessage;
-  private PopupComponent popupComponent;
-
-  public void setColor(Color color) {
-    this.color = color;
+  /**
+   * Highlights the component with the specified message. The highlighting is cleared after 2 seconds automatically.
+   *
+   * @param component the component to highlight
+   * @param message   the message to display
+   */
+  public static void error(JComponent component, String message) {
+    Highlighter highlighter = new Highlighter(component);
+    highlighter.setHighlightColor(Color.red.darker());
+    highlighter.highlight(message);
   }
 
-  public void setMargin(int margin) {
-    this.margin = margin;
+  /**
+   * Clears the highlighting of the component, if any.
+   *
+   * @param component the component
+   */
+  public static void clearHighlighting(JComponent component) {
+    getActiveHighlighter(component).ifPresent(Highlighter::clear);
+  }
+
+  private static final ScheduledExecutorService EXECUTOR_SERVICE = Executors.newScheduledThreadPool(10);
+  private static final Map<JComponent, Highlighter> HIGHLIGHTERS = new HashMap<>();
+
+  private final Border origBorder;
+  private final JComponent component;
+
+  private Color highlightColor;
+  private int highlightMargin;
+  private double duration;
+  private PopupComponent popupComponent;
+
+  /**
+   * Creates a new Highlighter. The highlighting is cleared after 2 seconds automatically. The highlighting color is by
+   * default blue.
+   *
+   * @param component the component
+   */
+  public Highlighter(JComponent component) {
+    if (component == null) {
+      throw new IllegalArgumentException("Component cannot be null");
+    }
+    this.component = component;
+    origBorder = component.getBorder();
+    highlightColor = Color.blue.darker();
+    highlightMargin = 2;
+    duration = 2.0;
   }
 
   /**
    * Sets the highlighting duration in seconds.
+   *
    * @param duration in seconds
    */
   public void setDuration(double duration) {
     this.duration = duration;
   }
 
-  public void setInfoMessage(String infoMessage) {
-    this.infoMessage = infoMessage;
+  /**
+   * Sets the color of the highlight border.
+   *
+   * @param highlightColor the color
+   */
+  public void setHighlightColor(Color highlightColor) {
+    this.highlightColor = highlightColor;
   }
 
-  public void highlight(JComponent component) {
-    oldBorder = component.getBorder();
-    theComponent = component;
-    theComponent.setBorder(new MatteBorder(margin, margin, margin, margin, color));
+  /**
+   * Sets the margin of the highlight border.
+   *
+   * @param highlightMargin the margin
+   */
+  public void setHighlightMargin(int highlightMargin) {
+    this.highlightMargin = highlightMargin;
+  }
+
+  /**
+   * Highlights the component with a colored border.
+   *
+   * @see #setHighlightColor
+   */
+  public void highlight() {
+    highlightComponent(null);
+  }
+
+  /**
+   * Highlights the component with a colored border and the specified message. If the message is null, the component is
+   * only highlighted.
+   *
+   * @param infoMessage the message, can be null
+   */
+  public void highlight(String infoMessage) {
+    highlightComponent(infoMessage);
+    HIGHLIGHTERS.put(component, this);
+    EXECUTOR_SERVICE.schedule(this::clear, (long) (duration * 1000), TimeUnit.MILLISECONDS);
+  }
+
+  /**
+   * Clears the highlighting of the associated component.
+   */
+  public void clear() {
+    if (component != null) {
+      clearComponent();
+      HIGHLIGHTERS.remove(component);
+    }
+  }
+
+  private void highlightComponent(String infoMessage) {
+    component.setBorder(createHighlightBorder(highlightMargin, highlightColor));
     if (infoMessage != null) {
       popupComponent = createPopup(infoMessage);
     }
+  }
 
-    ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-    executorService.schedule(this::removeHighlighting, (long) (duration * 1000), TimeUnit.MILLISECONDS);
+  private MatteBorder createHighlightBorder(int margin, Color color) {
+    return new MatteBorder(margin, margin, margin, margin, color);
+  }
+
+  private void clearComponent() {
+    component.setBorder(origBorder);
+    if (popupComponent != null) {
+      popupComponent.hidePopup();
+    }
   }
 
   private PopupComponent createPopup(String message) {
@@ -74,24 +166,16 @@ public class Highlighter {
     textField.setFont(textField.getFont().deriveFont(Font.BOLD));
     textField.setEditable(false);
     textField.setBorder(new EmptyBorder(0, 0, 0, 0));
-    float[] rgbComponents = color.getRGBComponents(null);
-    textField.setBackground(new Color(color.getColorSpace(), rgbComponents, rgbComponents[3] * 0.3f));
+    float[] rgbComponents = highlightColor.getRGBComponents(null);
+    textField.setBackground(new Color(highlightColor.getColorSpace(), rgbComponents, rgbComponents[3] * 0.3f));
     PopupComponent popupComponent = new PopupComponent(textField);
-    popupComponent.showPopupAt(theComponent.getLocationOnScreen().x,theComponent.getLocationOnScreen().y + theComponent.getHeight());
+    popupComponent.showPopupAt(component.getLocationOnScreen().x,
+        component.getLocationOnScreen().y + component.getHeight());
     return popupComponent;
   }
 
-  private void removeHighlighting() {
-    theComponent.setBorder(oldBorder);
-    if (popupComponent != null) {
-      popupComponent.hidePopup();
-    }
-  }
-
-  public static void error(JComponent component, String message) {
-    Highlighter highlighter = new Highlighter();
-    highlighter.setInfoMessage(message);
-    highlighter.highlight(component);
+  private static Optional<Highlighter> getActiveHighlighter(JComponent component) {
+    return Optional.ofNullable(HIGHLIGHTERS.get(component));
   }
 }
 
