1f662167873dcc48849975b287807e35f66145fd
Marco <info@eomasters.org>
2024-07-01 12:49:42 +0200

    Remove unused import in GsonStoreTest

--- src/test/java/org/eomasters/utils/GsonStoreTest.java
+++ src/test/java/org/eomasters/utils/GsonStoreTest.java
@@ -20,7 +20,6 @@ package org.eomasters.utils;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
